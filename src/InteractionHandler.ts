8e00768044ae98ac19e6d2abccc113e9c33ec764
OoP1nk <per.nmds@gmail.com>
2022-11-19 10:53:08 +0000

    [+Added] Command cooldowns

--- src/InteractionHandler.ts
+++ src/InteractionHandler.ts
@@ -1,4 +1,4 @@
-import type RestrictedCommand from "./interactions/commands/RestrictedCommand";
+import type { RestrictedCommand } from "./interactions/commands/RestrictedCommand";
 import type DIH4DJS from "./index";
 
 import path from "node:path";
@@ -6,21 +6,26 @@ import fs from "node:fs/promises";
 import { RegistrationType } from './interactions/commands/application/RegistrationType';
 import { ContextCommand } from "./interactions/commands/application/ContextCommand";
 import { SlashCommand } from "./interactions/commands/application/SlashCommand";
+import ComponentHandler from "./interactions/ComponentHandler";
 import CommandUtils from "./utils/CommandUtils";
 import DIH4DJSLogger from "./DIH4DJSLogger";
 import SmartQueue from "./SmartQueue";
 import Pair from "./utils/Pair";
+import ComponentIdBuilder from "./utils/ComponentIdBuilder";
 import { 
     ApplicationCommand,
     ApplicationCommandType,
     BaseInteraction, 
+    ButtonInteraction, 
     ChatInputCommandInteraction, 
     Collection, 
     Guild, 
     GuildMember, 
     MessageContextMenuCommandInteraction, 
+    ModalSubmitInteraction, 
     REST, 
     Routes, 
+    SelectMenuInteraction, 
     SlashCommandSubcommandBuilder, 
     SlashCommandSubcommandGroupBuilder, 
     UserContextMenuCommandInteraction 
@@ -52,6 +57,13 @@ export default class InteractionHandler {
      */
     private userContextIndex: Collection<string, ContextCommand.User>;
 
+    /**
+     * An index of all {@link ComponentHandler}s.
+     */
+    private buttonHandlers: Collection<string, ComponentHandler>;
+    private selectMenuHandlers: Collection<string, ComponentHandler>;
+    private modalHandlers: Collection<string, ComponentHandler>;
+
     private dih4djs: DIH4DJS;
 
     /**
@@ -70,6 +82,10 @@ export default class InteractionHandler {
         this.subcommandIndex = new Collection();
         this.messageContextIndex = new Collection();
         this.userContextIndex = new Collection();
+
+        this.buttonHandlers = new Collection();
+        this.selectMenuHandlers = new Collection();
+        this.modalHandlers = new Collection();
     }
 
     /**
@@ -83,8 +99,25 @@ export default class InteractionHandler {
         const token = this.dih4djs.getClient().token!;
         const appId = this.dih4djs.getClient().application!.id!;
         const rest = new REST({ version: '10' }).setToken(token);
+        this.dih4djs.getClient().guilds.cache.forEach(async (guild) => {
+            const existing = guild.commands.cache;
+            var guildData: Pair<SlashCommand[], ContextCommand<any>[]>;
+            if(guild.id === this.dih4djs.getTestGuild())
+                guildData = CommandUtils.filterByType(data, RegistrationType.Private);
+            else guildData = CommandUtils.filterByType(data, RegistrationType.Guild);
+            existing.forEach((e) => this.cacheCommand(e));
+            guildData = new SmartQueue(guildData.getFirst(), guildData.getSecond()).checkGuild(Array.from(existing.values()), guild);
+            if(guildData.getFirst() || guildData.getSecond()) {
+                this.upsertGuildCommands(guild, guildData.getFirst(), guildData.getSecond())
+                .then(() => 
+                    DIH4DJSLogger.info("Queued %s guild-only command(s): %ss"
+                    .replace("%s", `${guildData.getFirst().length + guildData.getSecond().length}`)
+                    .replace("%ss", CommandUtils.getNames(guildData.getSecond(), guildData.getFirst())))
+                );
+            }
+        });
         rest.get(Routes.applicationCommands(appId)).then((existing: any) => {
-            var globalData = CommandUtils.filterByType(data, RegistrationType.GLOBAL)
+            var globalData = CommandUtils.filterByType(data, RegistrationType.Global)
             existing.forEach((e: any) => this.cacheCommand((e as ApplicationCommand)));
             globalData = new SmartQueue(globalData.getFirst(), globalData.getSecond()).checkGlobal(existing);
             if(globalData.getFirst() || globalData.getSecond()) {
@@ -98,9 +131,16 @@ export default class InteractionHandler {
         });
     }
 
-    // private upsertGuildCommands(guild: Guild, slashCommands: SlashCommand[], contextCommands: ContextCommand<any>[]) {
-
-    // }
+    private async upsertGuildCommands(guild: Guild, slashCommands: SlashCommand[], contextCommands: ContextCommand<any>[]) {
+        const token = this.dih4djs.getClient().token!;
+        const appId = this.dih4djs.getClient().application!.id!;
+        const rest = new REST({ version: '10' }).setToken(token);
+        var commands: any[] = Array.of();
+        slashCommands.map(c => commands.push(c.getCommandData().toJSON()));
+        contextCommands.map(c => commands.push(c.getCommandData().toJSON()));
+        await rest.put(Routes.applicationGuildCommands(appId, guild.id), { body: commands })
+            .catch((reason) => DIH4DJSLogger.warn(reason));
+    }
 
     private async upsertGlobalCommands(slashCommands: SlashCommand[], contextCommands: ContextCommand<any>[]) {
         const token = this.dih4djs.getClient().token!;
@@ -139,16 +179,16 @@ export default class InteractionHandler {
             const stat = await fs.lstat(filePath);
             if (stat.isDirectory()) this.registerCommandsAndOrComponenets(path.join(pkg, file));
             if (file.endsWith('.js') || file.endsWith('.ts')) {
-                const { default: Command } = await import(filePath);
-                const command = new Command();
-                switch(true) {
-                    case command instanceof SlashCommand:
+                try {
+                    const { default: Command } = await import(filePath);
+                    const command = new Command();
+                    if(command instanceof SlashCommand) 
                         this.slashCommands.push(command);
-                        break;
-                    case command instanceof ContextCommand:
+                    if(command instanceof ContextCommand) 
                         this.contextCommands.push(command);
-                        break;
-                }
+                    if(command instanceof ComponentHandler)
+                        this.putComponentHandlers(command);
+                } catch (err) {DIH4DJSLogger.error(err)}
             }
         }
     }
@@ -258,38 +298,17 @@ export default class InteractionHandler {
         return data;
     }
 
-    // /**
-    //  * Finds and Registers all {@link ComponentHandler}s
-    //  * @param dir Location of {@link ComponentHandler}s
-    //  */
-    // private async findInteractionHandlers(dir: string = "../systems/"): Promise<void> {
-    //     const basePath = path.join(__dirname, dir);
-    //     const files = await fs.readdir(basePath);
-    //     for (const file of files) {
-    //         const filePath = path.join(basePath, file);
-    //         const stat = await fs.lstat(filePath);
-    //         if (stat.isDirectory()) this.findInteractionHandlers(path.join(dir, file));
-    //         if (file.endsWith('.js') || file.endsWith('.ts')) {
-    //             const { default: Handler } = await import(filePath);
-    //             const handler = new Handler();
-    //             if(handler instanceof ComponentHandler) {
-    //                 this.putComponentHandlers(handler);
-    //             }
-    //         }
-    //     }
-    // }
-
-    // /**
-    //  * Register all {@link ComponentHandler}s
-    //  * @param handler {@link ComponentHandler} instance
-    //  * @returns 
-    //  */
-    // private putComponentHandlers(handler?: ComponentHandler<any>): void {
-    //     if(handler === null) return;
-    //     handler?.getHandleButtonIds().forEach(s => this._handlerIndex.set(s, handler));
-    //     handler?.getHandleSelectMenuIds().forEach(s => this._handlerIndex.set(s, handler));
-    //     handler?.getHandleModalIds().forEach(s => this._handlerIndex.set(s, handler));
-    // }
+    /**
+     * Register all {@link ComponentHandler}s
+     * @param handler {@link ComponentHandler} instance
+     * @returns 
+     */
+    private putComponentHandlers(handler?: ComponentHandler): void {
+        if(handler === null) return;
+        handler?.getButtonIds().forEach(s => this.buttonHandlers.set(s, handler));
+        handler?.getSelectMenuIds().forEach(s => this.selectMenuHandlers.set(s, handler));
+        handler?.getModalIds().forEach(s => this.modalHandlers.set(s, handler));
+    }
 
     /**
      * Handle all {@link ChatInputCommandInteraction}s
@@ -305,10 +324,10 @@ export default class InteractionHandler {
             if(slashcommand === null) {
                 var base = subcommand.getParent()!;
                 if(this.passesRequirements(interaction, base) && this.passesRequirements(interaction, subcommand)) {
-                    subcommand?.execute(this.dih4djs.getClient(), interaction);
+                    subcommand?.onExecute(this.dih4djs.getClient(), interaction);
                 }
             } else if(this.passesRequirements(interaction, slashcommand)) {
-                slashcommand.execute(this.dih4djs.getClient(), interaction);
+                slashcommand.onExecute(this.dih4djs.getClient(), interaction);
             }
         }
     }
@@ -323,7 +342,7 @@ export default class InteractionHandler {
             throw new Error("UserContext command %s is not registered.".replace("%s", interaction.commandName));
         } else {
             if(this.passesRequirements(interaction, context)) {
-                context.execute(this.dih4djs.getClient(), interaction);
+                context.onExecute(this.dih4djs.getClient(), interaction);
             }
         }
     }
@@ -338,7 +357,7 @@ export default class InteractionHandler {
             throw new Error("MessageContext command %s is not registered.".replace("%s", interaction.commandName));
         } else {
             if(this.passesRequirements(interaction, context)) {
-                context.execute(this.dih4djs.getClient(), interaction);
+                context.onExecute(this.dih4djs.getClient(), interaction);
             }
         }
     }
@@ -347,41 +366,48 @@ export default class InteractionHandler {
      * Handle all {@link ButtonInteraction}s
      * @param interaction Button interaction response
      */
-    // public handleButton(interaction: ButtonInteraction) {
-    //     var component: ComponentHandler = this._handlerIndex.get(ComponentIdBuilder.split(interaction.customId)[0])!;
-    //     if(component === null) {
-    //         Bot.logger.warn("Button with the id %id% could not be found.".replace("%id%", interaction.customId))
-    //     } else {
-    //         component.handleButton(interaction);
-    //     }
-    // }
+    public handleButton(interaction: ButtonInteraction) {
+        var component = this.buttonHandlers.get(ComponentIdBuilder.split(interaction.customId)[0]!);
+        if(component === null || component === undefined) {
+            DIH4DJSLogger.warn("Button with the id %s could not be found.".replace("%s", interaction.customId))
+        } else {
+            component.handleButton(interaction);
+        }
+    }
 
     /**
      * Handle all {@link SelectMenuInteraction}s
      * @param interaction SelectMenu interaction response
      */
-    // public handleSelectMenu(interaction: SelectMenuInteraction) {
-    //     var component: ComponentHandler = this._handlerIndex.get(ComponentIdBuilder.split(interaction.customId)[0])!;
-    //     if(component === null) {
-    //         Bot.logger.warn("Select menu with the id %id% could not be found.".replace("%id%", interaction.customId));
-    //     } else {
-    //         component.handleSelectMenu(interaction);
-    //     }
-    // }
+    public handleSelectMenu(interaction: SelectMenuInteraction) {
+        var component = this.selectMenuHandlers.get(ComponentIdBuilder.split(interaction.customId)[0]!);
+        if(component === null || component === undefined) {
+            DIH4DJSLogger.warn("Select menu with the id %id% could not be found.".replace("%id%", interaction.customId));
+        } else {
+            component.handleSelectMenu(interaction);
+        }
+    }
 
     /**
      * Handle all {@link ModalSubmitInteraction}s
      * @param interaction Modal interaction response
      */
-    // public handleModal(interaction: ModalSubmitInteraction) {
-    //     var modal: ComponentHandler = this._handlerIndex.get(ComponentIdBuilder.split(interaction.customId)[0])!;
-    //     if(modal === null) {
-    //         Bot.logger.warn("Modal with the id %id% could not be found.".replace("%id%", interaction.customId));
-    //     } else {
-    //         modal.handleModal(interaction);
-    //     }
-    // }
+    public handleModal(interaction: ModalSubmitInteraction) {
+        var modal = this.modalHandlers.get(ComponentIdBuilder.split(interaction.customId)[0]!);
+        if(modal === null || modal === undefined) {
+            DIH4DJSLogger.warn("Modal with the id %id% could not be found.".replace("%id%", interaction.customId));
+        } else {
+            modal.handleModalSubmit(interaction);
+        }
+    }
 
+    /**
+     * Checks if the {@link BaseInteraction} passes the {@link RestrictedCommand}
+     * requirements
+     * @param interaction The {@link BaseInteraction}
+     * @param command The {@link RestrictedCommand} which contains the (possible) restrictions.
+     * @returns Whether the event was fired
+     */
     private passesRequirements(interaction: BaseInteraction, command: RestrictedCommand) {
         var userId = interaction.user.id;
         var userIds = command.getRequiredUsers();
@@ -393,6 +419,14 @@ export default class InteractionHandler {
             var member: GuildMember = (interaction.member as GuildMember);
             if(roleIds !== null && roleIds.length !== 0 && !member.roles.cache.find(r => roleIds.includes(r.id))) {
                 return false;
+            }
+        }
+        // Check if the command has enabled some sort of cooldown
+        if(command.getCommandCooldown() !== 0) {
+            if(command.hasCooldown(userId)) {
+                return false;
+            } else {
+                command.applyCooldown(userId, (Date.now() + command.getCommandCooldown()));
             }
         }
         return true;
--- src/interactions/commands/RestrictedCommand.ts
+++ src/interactions/commands/RestrictedCommand.ts
@@ -1,11 +1,33 @@
+import { Collection } from "discord.js";
+import ComponentHandler from "../ComponentHandler";
+
 /**
  * Represents a restricted command.
  */
-export default abstract class RestrictedCommand {
+export abstract class RestrictedCommand extends ComponentHandler {
+    private COOLDOWN_CACHE: Collection<string, RestrictedCommand.Cooldown> = new Collection();
 
+    private requiredGuilds: string[] = Array.of();
     private requiredPermissions: bigint[] = Array.of();
     private requiredUsers: string[] = Array.of();
     private requiredRoles: string[] = Array.of();
+    private commandCooldown: number = 0;
+
+    /**
+     * Allows to required a set of {@link Guild}s only in which the command can get executed.
+     * @param guilds An array of guild ids.
+     */
+    public setRequiredGuilds(...guilds: string[]) {
+        this.requiredGuilds = guilds;
+    }
+
+    /**
+     * The required guilds the command can be executed in.
+     * @returns The array of guild ids.
+     */
+    public getRequiredGuilds(): string[] {
+        return this.requiredGuilds;
+    }
 
     /**
      * The list of permissions that is required by the user.
@@ -53,5 +75,82 @@ export default abstract class RestrictedCommand {
      */
     public setRequiredRoles(...roles: string[]): void {
         this.requiredRoles = roles;
+    }
+
+    /**
+     * Allows to set cooldown for this command.
+     * @param duration The duration the user will have to wait in milliseconds
+     */
+    public setCommandCooldown(duration: number) {
+        this.commandCooldown = duration;
+    }
+
+    /**
+     * Returns the timestamp the user has to wait between command executions.
+     * @returns The timestamp
+     */
+    public getCommandCooldown(): number {
+        return this.commandCooldown;
+    }
+
+    /**
+     * Manually applys a cooldown for the specified user id.
+     * @param userId The targets' user id.
+     * @param nextUse The next time the command can be used.
+     */
+    public applyCooldown(userId: string, nextUse: number) {
+        this.COOLDOWN_CACHE.set(userId, new RestrictedCommand.Cooldown(Date.now(), nextUse));
+    }
+
+    /**
+     * Gets the cooldown when the specified user can execute the command again.
+     * If the user has not executed the command yet it will return a {@link RestrictedCommand.Cooldown}
+     * with both values {@link Date.now}
+     * @param userId The targets' user id
+     * @returns The timestamp at which the command can be executed again.
+     */
+    public retrieveCooldown(userId: string) {
+        var cooldown = this.COOLDOWN_CACHE.get(userId);
+        if(cooldown === null || cooldown === undefined) 
+            return new RestrictedCommand.Cooldown(Date.now(), Date.now());
+        return cooldown;
+    }
+
+    /**
+     * Returns whether the command can be used by the specified user.
+     * @param userId The target user id.
+     * @returns Whether the command can be executed.
+     */
+    public hasCooldown(userId: string): boolean {
+        const cooldown = this.retrieveCooldown(userId);
+        return cooldown.getLastUse() <= cooldown.getNextUse() ? true : false;
+    }
+}
+
+export namespace RestrictedCommand {
+    export class Cooldown {
+        private lastUse: number;
+        private nextUse: number;
+
+        constructor(lastUse: number, nestUse: number) {
+            this.lastUse = lastUse;
+            this.nextUse = nestUse;
+        }
+
+        /**
+         * Gets you the {@link Date} of when the user can use the {@link RestrictedCommand} the nest time.
+         * @returns The nest {@link Date} the command may be used again.
+         */
+        public getNextUse(): number {
+            return this.nextUse;
+        }
+
+        /**
+         * Gets you the {@link Date} the user has used {@link RestrictedCommand} the last time.
+         * @returns The last {@link Date} the command was used.
+         */
+        public getLastUse(): number {
+            return this.lastUse;
+        }
     }
 }
