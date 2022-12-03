94bf40d754f50f4e0d2f179e01d196bf9ce8f7d8
OoP1nk <per.nmds@gmail.com>
2022-12-03 18:33:20 +0000

    [Added] Component handler in interaction event

--- src/listeners/ActionInteractionCreate.js
+++ src/listeners/ActionInteractionCreate.js
@@ -14,10 +14,21 @@ class ActionInteractionCreate extends ActionListener {
      * @param {BaseInteraction} interaction 
      */
     async execute(dih4djs, interaction) {
+        /**
+         * Interaction Handlers
+         */
         const inManager = dih4djs.interactionManager;
         if (interaction.isChatInputCommand()) { inManager.handleSlashCommand(interaction); }
         if (interaction.isUserContextMenuCommand()) { inManager.handleUserContextCommand(interaction); }
         if (interaction.isMessageContextMenuCommand()) { inManager.handleMessageContextCommand(interaction); }
+
+        /**
+         * Component Handlers
+         */
+        const coManager = dih4djs.componentManager;
+        if (interaction.isButton()) { coManager.handleButton(interaction); }
+        if (interaction.isAnySelectMenu()) { coManager.handleSelectMenu(interaction); }
+        if (interaction.isModalSubmit()) { coManager.handleModal(interaction); }
     }
 }
 
